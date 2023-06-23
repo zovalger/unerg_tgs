35c641a9f2777b405493653f530f99355f492eea
zovalger <zovalger@gmail.com>
2023-06-23 12:08:37 -0400

    fix

--- src/pages/rutas/[_id]/index.jsx
+++ src/pages/rutas/[_id]/index.jsx
@@ -14,7 +14,6 @@ import BtnBus from "@/components/RouteView/infoBus/BtnBus";
 import HourBus from "@/components/RouteView/infoBus/HourBus";
 import BotonPaClient from "@/components/RouteView/clientView/BotonPaClient";
 
-
 import { IoIosArrowBack } from "react-icons/io";
 import { BiPencil } from "react-icons/bi";
 
@@ -32,153 +31,155 @@ import BusContext from "@/contexts/Bus.context";
 import { getTimetable_By_Id_Request } from "@/api/timetable.api";
 import ToastContext from "@/contexts/Toast.context";
 
-
 const MapView = dynamic(() => import("@/components/MapView_Leaflet/MapView"), {
-  ssr: false,
+	ssr: false,
 });
 
 //**********************************  Codigo  ************************//
 
 const RutaOverview = () => {
-  const router = useRouter();
-  const { _id } = router.query;
+	const router = useRouter();
+	const { _id } = router.query;
 
-  const { getRuta, insertRuta, setEditingRoute } = useContext(RutaContext);
-  const { getBuses_by_RutaId } = useContext(BusContext);
+	const { getRuta, insertRuta, setEditingRoute } = useContext(RutaContext);
+	const { getBuses_by_RutaId } = useContext(BusContext);
 
-
-
-
-  const {
+	const {
 		setCenterMap,
 		getCoordsDevice,
-		insertRuta:mapInsertRuta,
+		insertRuta: mapInsertRuta,
 		setUserCoord,
 		toogleViewUserCoord,
 	} = useContext(MapContext);
 	const { showErrorToast } = useContext(ToastContext);
 
-  const [timetable, setTimetable] = useState(null);
+	const [timetable, setTimetable] = useState(null);
 
-  const {
-    insertRuta: insertRutaMap,
-    insertWaypoint: insertWaypointMap,
-    insertBus: insertBusMap,
-    clearWaypoint,
-    clearRutas,
-  } = useContext(MapContext);
+	const {
+		insertRuta: insertRutaMap,
+		insertWaypoint: insertWaypointMap,
+		insertBus: insertBusMap,
+		clearWaypoint,
+		clearRutas,
+	} = useContext(MapContext);
 
-  const data = getRuta(_id);
+	const data = getRuta(_id);
 
-  useEffect(() => {
-    if (!data) {
-      if (_id)
-        getRuta_By_Id_Request(_id)
-          .then(({ data }) => {
-            insertRuta([data]);
-            insertRutaMap([data]);
-            insertWaypointMap(data.waypoints);
-          })
-          .catch((error) => console.log(error));
-    } else {
-      insertRutaMap([data]);
-      insertWaypointMap(data.waypoints);
-    }
+	useEffect(() => {
+		if (!data) {
+			if (_id)
+				getRuta_By_Id_Request(_id)
+					.then(({ data }) => {
+						insertRuta([data]);
+						insertRutaMap([data]);
+						insertWaypointMap(data.waypoints);
+					})
+					.catch((error) => console.log(error));
+		} else {
+			insertRutaMap([data]);
+			insertWaypointMap(data.waypoints);
+		}
 
-    insertBusMap(getBuses_by_RutaId(_id));
+		insertBusMap(getBuses_by_RutaId(_id));
 
-    if (_id && data)
-      getTimetable_By_Id_Request(data.timetableId)
-        .then(({ data }) => setTimetable(data))
-        .catch((error) => console.log(error));
-  }, []);
+		if (_id && data)
+			getTimetable_By_Id_Request(data.timetableId)
+				.then(({ data }) => setTimetable(data))
+				.catch((error) => console.log(error));
+	}, []);
 
-  const onClick = (_id) => {
-    console.log(_id);
-  };
+	const onClick = (_id) => {
+		console.log(_id);
+	};
 
-  const restore = () => {
-    clearWaypoint();
-    clearRutas();
-  };
+	const restore = () => {
+		clearWaypoint();
+		clearRutas();
+	};
 
-  return (
-    <Layout>
-      <div className="AppView">
-        {/* nav customizable */}
+	return (
+		<Layout>
+			<div className="AppView">
+				{/* nav customizable */}
 
-        <NavBar
-          left={
-            <>
-              <div>
-                <Link
-                  className={styleN.btn_return}
-                  href={"../map"}
-                  onClick={() => {
-                    restore();
-                  }}
-                >
-                  <IoIosArrowBack />
-                </Link>
-              </div>
-              <div className={styleN.title_nav}>
-                <h2>{data && data.name}</h2>
-              </div>
-            </>
-          }
-     
-        />
+				<NavBar
+					left={
+						<>
+							<div>
+								<Link
+									className={styleN.btn_return}
+									href={"../map"}
+									onClick={() => {
+										restore();
+									}}
+								>
+									<IoIosArrowBack />
+								</Link>
+							</div>
+							<div className={styleN.title_nav}>
+								<h2>{data && data.name}</h2>
+							</div>
+						</>
+					}
+				/>
 
-        {/* Contenedor del mapa */}
+				{/* Contenedor del mapa */}
 
-        <div className={`${"MapView__Container"} ${"MapView__ContainerRu"}`}>
-          <MapView />
-        </div>
+				<div className={`${"MapView__Container"} ${"MapView__ContainerRu"}`}>
+					<MapView />
+				</div>
 
-        <div className="container__rutas">
-          <div className={style.container_routes}>
-            {getBuses_by_RutaId(_id).map((b) => (
-              <BtnBus key={b._id} data={b} />
-            ))}
+				<div className="container__rutas">
+					<div className={style.container_routes}>
+						{getBuses_by_RutaId(_id).map((b) => (
+							<BtnBus key={b._id} data={b} />
+						))}
 
-            {timetable && <HourBus data={timetable} />}
-            <h2 style={{ textAlign: "center", marginTop: "15px", marginBottom:"15px" }}>
-              Paradas de autobus
-            </h2>
-            {data &&
-              data.waypoints.map((w) => (
-                <BotonPaClient data={w} key={uuid()} 
-                
-                onClick={async () => {
-                  const coordDevice = await getCoordsDevice();
-    
-                  if (!coordDevice)
-                    return showErrorToast(
-                      "No se pudo obtener la ubicacion del dispositivo"
-                    );
-    
-                  setUserCoord(coordDevice);
-                  toogleViewUserCoord(true);
-    
-                  mapInsertRuta([
-                    {
-                      _id: uuid(),
-                      status: "a",
-                      waypoints: [{ coord: coordDevice }, { coord: w.coord }],
-                    },
-                  ]);
-    router.push("../map")
-                  setTimeout(() => {
-                    setCenterMap(coordDevice, 16);
-                  }, 1000);
-                }}
-                />
-              ))}
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+						{timetable && <HourBus data={timetable} />}
+						<h2
+							style={{
+								textAlign: "center",
+								marginTop: "15px",
+								marginBottom: "15px",
+							}}
+						>
+							Paradas de autobus
+						</h2>
+						{data &&
+							data.waypoints.map((w) => (
+								<BotonPaClient
+									data={w}
+									key={uuid()}
+									onClick={async () => {
+										const coordDevice = await getCoordsDevice();
+
+										if (!coordDevice)
+											return showErrorToast(
+												"No se pudo obtener la ubicacion del dispositivo"
+											);
+
+										setUserCoord(coordDevice);
+										toogleViewUserCoord(true);
+
+										mapInsertRuta([
+											{
+												_id: uuid(),
+												status: "a",
+												waypoints: [{ coord: coordDevice }, { coord: w.coord }],
+											},
+										]);
+										router.push("../map");
+										setTimeout(() => {
+											setCenterMap(coordDevice, 16);
+										}, 1000);
+									}}
+								/>
+							))}
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default RutaOverview;
